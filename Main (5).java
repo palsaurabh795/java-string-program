c8466d3df15384f480473441cf10565df0091cbb
Saurabh Pal <pal.saurabhh@gmail.com>
2024-11-28 11:28:02 +0530

    Add files via upload

---
+++ Main (5).java
@@ -0,0 +1,18 @@
+Q.1 write a java program to convert a string to lowercase.
+// public class string {
+// public static void main(String[] args){
+//     String name = "jack parker";
+//     name = name.toLowerCase();
+//     System.out.println(name);
+// }
+// }
+// Q.2 write a java program to replace spaces with underscores.
+
+// public class string {
+//     public static void main(String[] args){
+//         String text = "to my friend";
+//         text = text.replace( " ",  "_");
+//         System.out.println(text);
+
+//     }
+// }
